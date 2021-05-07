d83193240fe3b06a19862100a960faa3166769a5
Santiago Salgado <jssalgadog@gmail.com>
2021-05-07 14:13:23 -0500

    finish some spiders

---
+++ Science_News/Science_News/spiders/howstuffworks.py
@@ -0,0 +1,94 @@
+import scrapy
+from ..items import ScienceNewsItem
+# Xpath
+
+# categories links = '//a[@data-text]/@href'
+# sub categories links = '//div[@data-track-gtm="Topics to Explore"]/div/div/a/@href'
+# news links = '//div[@class="mb-4"]/div[@data-track-gtm !="Topics to Explore"]//a/@href'
+# tags = '//div[@itemprop]/a/span/text()'
+# title = '//div[contains(@class,"page-title")]/h1/text()'
+# autor = '//span[@class="signature"]/text()'
+# image = '//img[@class="RichTextAlignCenter"]/@src'
+# image content = '//div[@class="new_text "]/p/span/text()'
+# date = '//span[@class="date"]/text()'
+# content = '//div[@class="new_text "]/p/span[notow]/text()'
+
+
+class SpiderHowStuffworks(scrapy.Spider):
+    name = 'howstuffworks'
+    start_urls = [
+        'https://www.howstuffworks.com'
+    ]
+    customs_settings = {
+        'FEED_URI': 'howstuffworks.json',
+        'FEED_FORMAT': 'json',
+        'FEED_EXPORT_ENCODING': 'utf-8'
+    }
+
+    def parse(self, response):
+        links_categories = response.xpath(
+            '//a[@data-text]/@href'
+            ).getall()
+        for link in links_categories:
+            yield response.follow(link, callback=self.parse_category)
+
+    def parse_category(self, response, **kwargs):
+        links_subcategories = response.xpath(
+            '//div[@data-track-gtm="Topics to Explore"]/div/div/a/@href'
+            ).getall()
+        for link in links_subcategories:
+            yield response.follow(link, callback=self.parse_notice)
+
+    def parse_notice(self, response, **kwargs):
+        news_links = response.xpath(
+            '//div[@class="mb-4"]/div[@data-track-gtm !="Topics to Explore"]//a/@href'
+            ).getall()
+        for link in news_links:
+            yield response.follow(link, callback=self.parse_link,
+                                    cb_kwargs= {'url': link})
+
+    def parse_link(self, response, **kwargs):
+        items = ScienceNewsItem()
+        link = kwargs['url']
+        title = response.xpath(
+            '//div[contains(@class,"page-title")]/h1/text()'
+        ).get()
+        title = title.strip('\n').strip('\t')
+        tag = response.xpath(
+            '//div[@itemprop]/a/span/text()'
+        ).getall()
+        author = response.xpath(
+            '//p[@class="mb-4"]/a/text()'
+        ).get()
+        image = response.xpath(
+            '//div[@class="media-hero-wrap"]//img/@src'
+        ).getall()
+        image = image[1]
+        image_content = response.xpath(
+            '//div[@class="media-body"]/text()'
+        ).getall()
+        imagen_content = response.xpath(
+            '//div[@class="media-body"]/text()'
+        ).getall()
+        imagecontent = []
+        for con in imagen_content:
+            imagecontent.append(con.replace('\n','').replace('\t',''))
+        date = response.xpath(
+            '//p[@class="mb-4"]/span/text()'
+        ).get()
+        content = response.xpath(
+            '//div[@class = "page-content"]//p/descendant-or-self::text()'
+        ).getall()
+        content_complete = ""
+        for cont in content:
+            content_complete += cont
+        items['url'] = link
+        items['tag'] = tag[1:]
+        items['title'] = title
+        items['author'] = author
+        items['image'] = image
+        items['image_content'] = imagecontent
+        items['date'] = date
+        items['content'] = content_complete.strip('Advertisement')
+
+        yield items
--- Science_News/Science_News/spiders/muyinteresante.py
+++ Science_News/Science_News/spiders/muyinteresante.py
@@ -13,25 +13,25 @@ from ..items import ScienceNewsItem
 # content = '//div[@class="new_text "]/p/span[notow]/text()'
 
 
-class SpiderNoticiasDeLaCiencia(scrapy.Spider):
-    name = 'noticiasdelaciencia'
+class MuyInteresante(scrapy.Spider):
+    name = 'muyinteresante'
     start_urls = [
-        'https://noticiasdelaciencia.com'
+        'https://www.muyinteresante.es'
     ]
     customs_settings = {
-        'FEED_URI': 'noticiasdelaciencia.json',
+        'FEED_URI': 'muyinteresant.json',
         'FEED_FORMAT': 'json',
         'FEED_EXPORT_ENCODING': 'utf-8'
     }
 
     def parse(self, response):
-        links_categories = response.xpath('//ul[@class="topmenu"]/li/a/@href').getall()
+        links_categories = response.xpath('//li[@class="dropdown "]/a/@href').getall()
         for link in links_categories:
             yield response.follow(link, callback=self.parse_category,
                                     cb_kwargs= {'url': response.urljoin(link)})
 
     def parse_category(self, response, **kwargs):
-        news_links = response.xpath('//h1[@class="title"]/a/@href').getall()
+        news_links = response.xpath('//div[@class="box--text--container"]/h2/a/@href').getall()
         for link in news_links:
             yield response.follow(link, callback=self.parse_link,
                                     cb_kwargs= {'url': response.urljoin(link)})
@@ -40,36 +40,44 @@ class SpiderNoticiasDeLaCiencia(scrapy.Spider):
         items = ScienceNewsItem()
         link = kwargs['url']
         title = response.xpath(
-            '//h1[@class="new_title"]/text()'
+            '//h1[contains(@class, "title")]/text()'
         ).get()
         tag = response.xpath(
-            '//h4[@class="new_pretitle"]/text()'
-        ).get()
+            '//h3[contains(@class, "tags")]//a/text()'
+        ).getall()
+        tags = []
+        for i in tag:
+            tags.append(i.strip('#'))
         author = response.xpath(
-            '//span[@class="signature"]/text()'
+            '//div[contains(@class, "author")]//h3/text()'
         ).get()
         image = response.xpath(
-            '//img[@class="RichTextAlignCenter"]/@src'
+            '//figure[@class]/img/@src'
         ).get()
         image_content = response.xpath(
-            '//div[@class="new_text "]/p/span/text()'
-        ).getall()
+            '//div[@class="paragraph--image--caption"]/text()'
+        ).get()
+        image_sumary = ''
+        image_sumary = response.xpath(
+            '//div[@class="paragraph--image--caption"]/text()'
+        ).get()
+        imagecontent = image_sumary + " " + image_content
         date = response.xpath(
-            '//span[@class="date"]/text()'
+           '//time[@class]/text()'
         ).get()
         content = response.xpath(
-            '//div[@class="new_text "]/descendant-or-self::text()'
+            '//div[@class="paragraph--text"]/*//text()'
         ).getall()
         content_complete = ""
         for cont in content:
             if cont != '\xa0':
                 content_complete += cont
         items['url'] = link
-        items['tag'] = tag
+        items['tag'] = tags
         items['title'] = title
         items['author'] = author
         items['image'] = image
-        items['image_content'] = image_content
+        items['image_content'] = imagecontent
         items['date'] = date
         items['content'] = content_complete
 
